2414eaa409a194c23b3ecb9c4c6daffeb15afe02
popolozer <129201068+popolozer@users.noreply.github.com>
2023-09-20 08:36:00 +0200

    Delete FStream/FStream directory

--- FStream/FStream/src/main/kotlin/com/lagradost/DoodExtractor.kt
+++
@@ -1,6 +0,0 @@
-package com.lagradost
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-
-class DoodsProExtractor : DoodLaExtractor() {
-    override var mainUrl = "https://doods.pro"
-}
